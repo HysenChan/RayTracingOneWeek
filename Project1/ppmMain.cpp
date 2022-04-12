edddfcdb68a4e772b12188733a9b4f161f13295b
HysenChan <HysenChan@163.com>
2022-04-12 14:35:17 +0800

    [change]带进度报告的主渲染循环。

--- Project1/ppmMain.cpp
+++ Project1/ppmMain.cpp
@@ -12,6 +12,8 @@ void CreatePPMFile()
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = image_height - 1; j >= 0; --j)
+	{
+		std::cerr << "\rScanlines remaining:" << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
 		{
 			auto r = double(i) / (image_width - 1);
@@ -24,6 +26,8 @@ void CreatePPMFile()
 
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
+	}
+	std::cerr << "\nDone.\n";
 }
 #pragma endregion
 
