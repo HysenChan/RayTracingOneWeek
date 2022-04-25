9b68da8eb440acee359813059a031af856cdfc8e
HysenChan <HysenChan@163.com>
2022-04-25 18:12:01 +0800

    [add]抗锯齿。

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,6 +3,7 @@
 #include"color.h"
 #include"hittable_list.h"
 #include"sphere.h"
+#include"camera.h"
 
 #include<iostream>
 
@@ -36,6 +37,7 @@ void CreatePPMFile()
 	//Image
 	const int image_width = 256;
 	const int image_height = 256;
+	const int samples_per_pixel = 100;
 
 	//Render
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -55,7 +57,7 @@ void CreatePPMFile()
 
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';*/
 			color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
-			write_color(std::cout, pixel_color);
+			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 	std::cerr << "\nDone.\n";
@@ -69,6 +71,7 @@ void CreateRay()
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 100;
 
 	//World
 	hittable_list world;
@@ -76,15 +79,7 @@ void CreateRay()
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	//Camera
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
-
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	//计算眼睛->屏幕中心的向量
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);//(-2,-1.125,-1)
+	camera cam;
 
 	//Render
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -94,11 +89,16 @@ void CreateRay()
 		std::cerr << "\rScanlines remaining:" << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
 		{
-			auto u = double(i) / (image_width - 1);
-			auto v = double(j) / (image_height - 1);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-			color pixel_color = ray_color(r,world);
-			write_color(std::cout, pixel_color);
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s)
+			{
+				auto u = double(i+random_double()) / (image_width - 1);
+				auto v = double(j+random_double()) / (image_height - 1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world);
+
+			}
+			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 	std::cerr << "\nDone.\n";
