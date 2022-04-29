e84fd7196959e1a401c21e006f4402d35d399eb7
HysenChan <HysenChan@163.com>
2022-04-29 17:37:58 +0800

    [add]景深球体。

--- Project1/camera.h
+++ Project1/camera.h
@@ -5,24 +5,38 @@ class camera
 {
 public:
 	camera(
+		point3 lookfrom,
+		point3 lookat,
+		vec3 vup,
 		double vfov,
-		double aspect_ratio
+		double aspect_ratio,
+		double sperture,
+		double focus_dist
 	) {
 		auto theta = degress_to_radians(vfov);
 		auto h = tan(theta / 2);
 		//auto aspect_ratio = 16.0 / 9.0;
 		auto viewport_height = 2.0 * h;
 		auto viewport_width = aspect_ratio * viewport_height;
-		auto focal_length = 1.0;
 
-		origin = point3(0, 0, 0);
-		horizontal = vec3(viewport_width, 0.0, 0.0);
-		vertical = vec3(0.0, viewport_height, 0.0);
-		lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+		auto w = unit_vector(lookfrom - lookat);
+		auto u = unit_vector(cross(vup, w));
+		auto v = cross(w, u);
+		//auto focal_length = 1.0;
+
+		origin = lookfrom;
+		horizontal = focus_dist * viewport_width * u;
+		vertical = focus_dist * viewport_height * v;
+		lower_left_corner = origin - horizontal / 2 - vertical / 2 - focus_dist * w;
+
+		lens_radius = sperture / 2;
 	};
-	ray get_ray(double u, double v)const
+	ray get_ray(double s, double t)const
 	{
-		return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+		vec3 rd = lens_radius * random_in_unit_disk();
+		vec3 offset = u * rd.x() + v * rd.y();
+
+		return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin);
 	}
 
 private:
@@ -30,4 +44,6 @@ private:
 	point3 lower_left_corner;
 	vec3 horizontal;
 	vec3 vertical;
+	vec3 u, v, w;
+	double lens_radius;
 };
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -87,30 +87,36 @@ void CreateRay()
 	auto R = cos(pi / 4);
 	hittable_list world;
 
-	auto material_left = make_shared<lambertian>(color(0, 0, 1));
-	auto material_right = make_shared<lambertian>(color(1, 0, 0));
+	//auto material_left = make_shared<lambertian>(color(0, 0, 1));
+	//auto material_right = make_shared<lambertian>(color(1, 0, 0));
 
-	world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
-	world.add(make_shared<sphere>(point3(R, 0, -1), R, material_right));
+	//world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
+	//world.add(make_shared<sphere>(point3(R, 0, -1), R, material_right));
 
-	//auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
 	//auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
 	//auto material_center = make_shared<dielectric>(1.5);
-	//auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
 	//auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-	//auto material_left = make_shared<dielectric>(1.5);
+	auto material_left = make_shared<dielectric>(1.5);
 	//auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
-	//auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-	//world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-	//world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-	//world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-	//world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
-	//world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	// Camera
 
-	camera cam(90.0, aspect_ratio);
+	point3 lookfrom(3, 3, 2);
+	point3 lookat(0, 0, -1);
+	vec3 vup(0, 1, 0);
+	auto dist_to_focus = (lookfrom - lookat).length();
+	auto aperture = 2.0;
+
+	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
 	// Render
 
